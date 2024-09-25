513b23ee5c4479b0d833b27e2e11be47a946193d
kezuk-dev <Ytolimc040986&@*>
2024-09-25 18:37:22 +0200

    edit like to ux

--- client/src/components/editing/EditUser.jsx
+++ client/src/components/editing/EditUser.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateUser } from "../../reducer/user.reducer.js";
+import "./edit.css"
 
 function UserEdit() {
   const userData = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [isEditing, setIsEditing] = useState(false);
-  const [editedButtonName, setEditedButtonName] = useState("Edit Name");
-  const inputRef = useRef(null);
+  const [formData, setFormData] = useState({
+    userName: userData.userName || "",
+    firstName: userData.firstName || "",
+    lastName: userData.lastName || "",
+  });
+  const inputRefs = {
+    userName: useRef(null),
+    firstName: useRef(null),
+    lastName: useRef(null),
+  };
 
   let token = "";
   const getToken = () => {
@@ -21,71 +30,104 @@ function UserEdit() {
 
   const handleEditButtonClick = () => {
     setIsEditing(true);
-    setEditedButtonName(userData.userName);
   };
 
-  const handleButtonNameChange = (e) => {
-    setEditedButtonName(e.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
-  const handleInputBlur = async () => {
+  const handleSaveClick = async () => {
     setIsEditing(false);
     try {
-      const response = await fetch(
-        "http://localhost:3001/api/v1/user/profile",
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            userName: editedButtonName,
-          }),
-        }
-      );
+      const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          userName: formData.userName,
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+        }),
+      });
       if (response.ok) {
-        dispatch(updateUser({ userName: editedButtonName }));
+        dispatch(updateUser(formData));
       } else {
-        console.error("Error lors de l'édition du user name");
+        console.error("Error during user info update.");
       }
     } catch (error) {
-      console.error("Error lors de l'édition du user name");
+      console.error("Error during user info update.");
     }
-    setEditedButtonName("Edit Name");
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    setFormData({
+      userName: userData.userName,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+    });
   };
 
   useEffect(() => {
     if (isEditing) {
-      inputRef.current.focus();
+      inputRefs.userName.current.focus();
     }
   }, [isEditing]);
 
   return (
-    <div className="header">
-      <h1>
-        Welcome back
-        <br />
-        {userData.firstName} {userData.lastName}!
-      </h1>
+    <div className="user-edit-container">
       {isEditing ? (
-        <input
-          ref={inputRef}
-          type="text"
-          value={editedButtonName}
-          onChange={handleButtonNameChange}
-          onBlur={handleInputBlur}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              e.preventDefault();
-              handleInputBlur();
-            }
-          }}
-          className="input-button"
-        />
+        <form className="edit-form">
+          <div className="form-group">
+            <label htmlFor="userName">User name:</label>
+            <input
+              ref={inputRefs.userName}
+              type="text"
+              id="userName"
+              name="userName"
+              value={formData.userName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="firstName">First name:</label>
+            <input
+              ref={inputRefs.firstName}
+              type="text"
+              id="firstName"
+              name="firstName"
+              disabled
+              value={formData.firstName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Last name:</label>
+            <input
+              ref={inputRefs.lastName}
+              type="text"
+              id="lastName"
+              name="lastName"
+              disabled
+              value={formData.lastName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="button-group">
+            <button type="button" className="save-button" onClick={handleSaveClick}>
+              Save
+            </button>
+            <button type="button" className="cancel-button" onClick={handleCancelClick}>
+              Cancel
+            </button>
+          </div>
+        </form>
       ) : (
         <button className="edit-button" onClick={handleEditButtonClick}>
-          {editedButtonName}
+          Edit Name
         </button>
       )}
     </div>
